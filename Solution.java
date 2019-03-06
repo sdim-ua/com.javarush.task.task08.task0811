2fc329c7a16c163f8f0f1680f79c5330e8e16750
sdim-ua <ingersol@yandex.ua>
2019-03-06 23:36:33 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,42 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Квартет «Методы»
+*/
+
+public class Solution {
+    public static List getListForGet() {
+        //напишите тут ваш код
+         ArrayList list = new ArrayList();
+        return ArrayList;
+
+    }
+
+    public static List getListForSet() {
+        //напишите тут ваш код
+         ArrayList list = new ArrayList();
+         return ArrayList;
+
+    }
+
+    public static List getListForAddOrInsert() {
+        //напишите тут ваш код
+        LinkedList list  = new LinkedList<>();
+         return ArrayList;
+
+    }
+
+    public static List getListForRemove() {
+        //напишите тут ваш код
+        LinkedList list  = new LinkedList<>();
+         return LinkedList;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
